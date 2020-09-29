3dd8c69260ce9fb8f7931a98addf6afb07deea7e
karthykarthick <karthylock@gmail.com>
2020-09-29 21:48:17 +0530

    add new files

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,38 +1,46 @@
-
-
 class ArticlesController < ApplicationController
-    include ArticlesHelper
-  
-    #...
-  
-    def create
-      @article = Article.new(article_params)
-      @article.save
-  
-      redirect_to article_path(@article)
-    end
+  include ArticlesHelper
+  def index
+    @articles = Article.all
+  end
 
-    def index
-        @articles = Article.all
-      end
+  def show
+    @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
+  end
 
-      def show
-        @article = Article.find(params[:id])
-      end
-      def new
-        @article = Article.new
+  def new
+    @article = Article.new
+  end
 
-      end
-      def edit
-        @article = Article.find(params[:id])
-      end
+  def create
+    @article = Article.new(article_params)
+    @article.save
 
-      def update
-        @article = Article.find(params[:id])
-        @article.update(article_params)
-      
-        flash.notice = "Article '#{@article.title}' Updated!"
-      
-        redirect_to article_path(@article)
-      end
+    flash.notice = "Article '#{@article.title}' Created!"
+    redirect_to @article
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash.notice = "Article '#{@article.title}' Deleted!"
+
+    redirect_to @article
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to @article
+  end
 end
