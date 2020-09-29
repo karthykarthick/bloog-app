02f5e7b2db04b862a94380f8f7abac1c54c2d1d9
karthykarthick <karthylock@gmail.com>
2020-09-29 21:17:20 +0530

    add style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,17 @@
+
+
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+  
+    #...
+  
+    def create
+      @article = Article.new(article_params)
+      @article.save
+  
+      redirect_to article_path(@article)
+    end
+
     def index
         @articles = Article.all
       end
@@ -7,6 +20,7 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
       end
       def new
+        @article = Article.new
 
       end
 end
