17c2f314e27275b56d792afd8437b20a08ed73bc
karthykarthick <karthylock@gmail.com>
2020-09-29 21:46:03 +0530

    add controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,4 +23,16 @@ class ArticlesController < ApplicationController
         @article = Article.new
 
       end
+      def edit
+        @article = Article.find(params[:id])
+      end
+
+      def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+      
+        flash.notice = "Article '#{@article.title}' Updated!"
+      
+        redirect_to article_path(@article)
+      end
 end
